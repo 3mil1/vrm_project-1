8757b3b8c98c6671f8d07ccb1df38bcd0cf1b72b
Anna Karutina <anna.karutina@khk.ee>
2020-10-06 10:43:17 +0300

    Parandatud formis tühja välju kasutamine

--- src/Controller/BookingIndex.php
+++ src/Controller/BookingIndex.php
@@ -70,7 +70,8 @@ class BookingIndex extends AbstractController
                 'required' => true
             ])
             ->add('additionalInformation', TextareaType::class, [
-                'required' => false
+                'required' => false,
+                'empty_data' => 'no additional information'
             ])
             ->add('submit', SubmitType::class, ['label' => 'Create Bookings'])
             ->getForm();
